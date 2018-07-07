8d019aca2cec2646f9c822235e30800b4da617d1
unknown <gburd14@freeuni.edu.ge>
2018-07-07 07:42:36 +0400

    comments edited

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
-// and then call `Vue.use(VueRouter)`.
 
-// 1. Define route components.
-// These can be imported from other files
 const Homepage = Vue.component('Blogs', {
     template: '#blogs-block-template'
 });
@@ -22,26 +18,17 @@ const Blog = Vue.component('Blog', {
 });
 
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
 const routes = [
   { path: '/', component: Homepage },
   { path: '/blog', component: Blog }
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
+
 const router = new VueRouter({
   routes // short for `routes: routes`
 })
 
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
+
 const app = new Vue({
   router
 }).$mount('#app')
@@ -58,7 +45,6 @@ function login(){
 	window.open("http://172.16.0.164:8080/homepage.html", "_self");
 }
 
-// Now the app has started!
 
 document.addEventListener('DOMContentLoaded', function () {
    [].forEach.call(document.querySelectorAll('.scroll-button'), function(item) {
